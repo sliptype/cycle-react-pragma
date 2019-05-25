ae26c006728bd9f786eca8f29288e7b9f25e15e4
sliptype <sliptype@gmail.com>
2019-05-24 21:04:43 -0500

    Add more tests

--- test/jsx-factory.tsx
+++ test/jsx-factory.tsx
@@ -1,9 +1,11 @@
-import xs from 'xstream';
+const assert = require('assert');
+import React  from 'react';
 import {ReactSource} from '@cycle/react';
 import {makeDOMDriver} from '@cycle/react-dom';
 import {run} from '@cycle/run';
+import xs  from 'xstream';
+
 import CycleReactPragma from '../src/index';
-const assert = require('assert');
 
 function createRenderTarget(id: string | null = null) {
   const element = document.createElement('div');
@@ -15,7 +17,7 @@ function createRenderTarget(id: string | null = null) {
   return element;
 }
 
-describe('hyperscript helpers', function() {
+describe('jsx-factory', function() {
   it('w/ nothing', done => {
     function main(sources: {react: ReactSource}) {
       return {
@@ -81,4 +83,151 @@ describe('hyperscript helpers', function() {
       done();
     }, 100);
   });
+
+  it('w/ props', done => {
+    function main(sources: {react: ReactSource}) {
+      return {
+        react: xs.of(<section data-foo="bar"/>),
+      };
+    }
+
+    const target = createRenderTarget();
+    run(main, {
+      react: makeDOMDriver(target),
+    });
+
+    setTimeout(() => {
+      const section = target.querySelector('section') as HTMLElement;
+      assert.strictEqual(!!section, true);
+      assert.strictEqual(section.dataset.foo, 'bar');
+      done();
+    }, 100);
+  });
+
+  it('w/ props and children', done => {
+    function main(sources: {react: ReactSource}) {
+      return {
+        react: xs.of(
+          <section data-foo="bar">
+            <h1>heading 1</h1>
+            <h2>heading 2</h2>
+            <h3>heading 3</h3>
+          </section>
+        ),
+      };
+    }
+
+    const target = createRenderTarget();
+    run(main, {
+      react: makeDOMDriver(target),
+    });
+
+    setTimeout(() => {
+      const section = target.querySelector('section') as HTMLElement;
+      assert.strictEqual(!!section, true);
+      assert.strictEqual(section.dataset.foo, 'bar');
+      assert.strictEqual(section.children.length, 3);
+      done();
+    }, 100);
+  });
+
+  it('w/ className', done => {
+    function main(sources: {react: ReactSource}) {
+      return {
+        react: xs.of(<section className="foo"/>),
+      };
+    }
+
+    const target = createRenderTarget();
+    run(main, {
+      react: makeDOMDriver(target),
+    });
+
+    setTimeout(() => {
+      const section = target.querySelector('section') as HTMLElement;
+      assert.strictEqual(!!section, true);
+      assert.strictEqual(section.className, 'foo');
+      done();
+    }, 100);
+  });
+
+  it('w/ symbol selector', done => {
+    function main(sources: {react: ReactSource}) {
+
+      const inc = Symbol();
+      const inc$ = sources.react.select(inc).events('click');
+      const count$ = inc$.fold((acc: number, x: any) => acc + 1, 0);
+      const vdom$ = count$.map((i: number) => (
+        <div>
+          <h1>{'' + i}</h1>
+          <button sel={inc}/>
+        </div>
+      ));
+
+      return {react: vdom$};
+    }
+
+    const target = createRenderTarget();
+    run(main, {
+      react: makeDOMDriver(target),
+    });
+
+    setTimeout(() => {
+      const button = target.querySelector('button') as HTMLElement;
+      const h1 = target.querySelector('h1') as HTMLElement;
+      assert.strictEqual(!!button, true);
+      assert.strictEqual(!!h1, true);
+      assert.strictEqual(h1.innerHTML, '0');
+      button.click();
+      setTimeout(() => {
+        assert.strictEqual(h1.innerHTML, '1');
+        done();
+      }, 100);
+    }, 100);
+  });
+
+  it('renders functional component', done => {
+    const Test = () => (<h1>Functional</h1>);
+
+    function main() {
+      const vdom$ = xs.of(<Test/>);
+      return {react: vdom$};
+    }
+
+    const target = createRenderTarget();
+    run(main, {
+      react: makeDOMDriver(target),
+    });
+
+    setTimeout(() => {
+      const h1 = target.querySelector('h1') as HTMLElement;
+      assert.strictEqual(!!h1, true);
+      done();
+    }, 100);
+  });
+
+  it('renders class component', done => {
+    class Test extends React.Component {
+      render() {
+        return (<h1>Class</h1>);
+      }
+    }
+
+    function main() {
+      const vdom$ = xs.of(<Test/>);
+      return {react: vdom$};
+    }
+
+    const target = createRenderTarget();
+    run(main, {
+      react: makeDOMDriver(target),
+    });
+
+    setTimeout(() => {
+      const h1 = target.querySelector('h1') as HTMLElement;
+      assert.strictEqual(!!h1, true);
+      done();
+    }, 100);
+  });
+
 });
